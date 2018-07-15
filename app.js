8875ec4bbde250d38bedb7a7ef28dbc5e4541d5b
Jacob Boisseau <jacobboisseau@gmail.com>
2018-07-14 20:43:09 -0400

    started adding email files

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const app = express();
+const request = require('request');
 require('dotenv').config({ silent: true });
+const sendMessage = require('./index.js');
+
 const AppPort = process.env.PORT;
 const APIKEY = process.env.APIKEY;
-const request = require('request');
+
+const ORIGIN = '38.9552958,-77.3665917'; //latitude and longitude
+const DESTINATION = '38.8677246,-77.1815755'; //latitude and longitude
+const DEPARTURE_TIME = 'now'; // departure_time=now
+const THINGS_TO_AVOID = 'avoid=tolls'; //avoid=tolls|highways|ferries
+
 app.get('/', (req, res) => {
   request.get(
     {
-      url: `https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood4&key=${APIKEY}`
+      url: `https://maps.googleapis.com/maps/api/directions/json?origin=${ORIGIN}&destination=${DESTINATION}&${THINGS_TO_AVOID}&departure_time=${DEPARTURE_TIME}&key=${APIKEY}`
     },
     (err, response, body) => {
       if (body) {
         let jsonObj = JSON.parse(body);
-        console.log(jsonObj.routes[0].summary);
-        res.send(jsonObj.routes);
+        let resInfo = `
+        beep boop here's today's traffic information:
+
+        Best route for today is - ${jsonObj.routes[0].summary}.
+        It should take you ${jsonObj.routes[0].legs[0].duration.text}.
+
+        `;
+        console.log(resInfo);
+        console.log(sendMessage);
+        res.send(jsonObj);
       } else {
         let jsonObj = JSON.parse(err);
         res.send(jsonObj);
