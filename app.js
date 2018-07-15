8f2ddce93b6953ee92252a7c4627434add9e2614
Jacob Boisseau <jacobboisseau@gmail.com>
2018-07-15 16:38:52 -0400

    added link for app navigation

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ const app = express();
 const request = require('request');
 require('dotenv').config({ silent: true });
 const pb = require('./PushBullet.js').PushBullet;
+const CONFIG = require('./config.json');
 
 const AppPort = process.env.PORT;
 const GMAPS_APIKEY = process.env.GMAPS_APIKEY;
 
-const ORIGIN = '38.9552958,-77.3665917'; //latitude and longitude
-const DESTINATION = '38.8677246,-77.1815755'; //latitude and longitude
-const DEPARTURE_TIME = 'now'; // departure_time=now
-const THINGS_TO_AVOID = 'avoid=tolls'; //avoid=tolls|highways|ferries
-
+const ORIGIN = CONFIG.ORIGIN; //latitude and longitude
+const DESTINATION = CONFIG.DESTINATION; //latitude and longitude
+const DEPARTURE_TIME = CONFIG.DEPARTURE_TIME; // departure_time=now
+const THINGS_TO_AVOID = CONFIG.THINGS_TO_AVOID; //avoid=tolls|highways|ferries
+const URL = `https://maps.googleapis.com/maps/api/directions/json?origin=${ORIGIN}&destination=${DESTINATION}&${THINGS_TO_AVOID}&departure_time=${DEPARTURE_TIME}&key=${GMAPS_APIKEY}`;
+const NAV_LINK = `https://www.google.com/maps/dir/?api=1&origin=${ORIGIN}&destination=${DESTINATION}`;
 app.get('/', (req, res) => {
   request.get(
     {
-      url: `https://maps.googleapis.com/maps/api/directions/json?origin=${ORIGIN}&destination=${DESTINATION}&${THINGS_TO_AVOID}&departure_time=${DEPARTURE_TIME}&key=${GMAPS_APIKEY}`
+      url: URL
     },
     (err, response, body) => {
       if (body) {
@@ -24,13 +26,15 @@ app.get('/', (req, res) => {
 Here's today's traffic information:
 
 Best route for today is - ${jsonObj.routes[0].summary}.
-It should take you ${jsonObj.routes[0].legs[0].duration.text}.`;
+It should take you ${jsonObj.routes[0].legs[0].duration.text}.
+
+Link: ${NAV_LINK}`;
         console.log('Request received sending this update: ', resInfo);
 
         pb.push(
           'note',
-          undefined,
-          'jacobboisseau@gmail.com',
+          CONFIG.DEVID,
+          CONFIG.EMAIL,
           { title: 'Traffic Update', body: resInfo },
           undefined
         );
